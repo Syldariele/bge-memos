aeaa33ac2f0836d4afeebe05f53d1949c9561ea1
Olivier Mangin <olivier@make-it.fr>
2020-03-18 16:20:43 +0100

    add variables.js

---
+++ variables.js
@@ -0,0 +1,36 @@
+// Déclaration de variables
+
+let nbOfScreenColumns = 2;
+nbOfScreenColumns = "two";
+
+// Déclaration de variable constantes
+
+const nbOfTry = 3;
+// nbOfTry = 5 -> génère une erreur
+
+
+
+// types de variables
+
+const strVariable = "chaine de caractère";
+const numberVariable = 3.14;
+const booleanVariable = true;
+const arrayVariable = [
+    "cookies",
+    3310,
+    true,
+    [],
+];
+const objectVariable = {
+    favoriteBiscuit: "cookies",
+    favoriteNokia: 3310,
+    haveGoodJokes: true,
+    friends: [],
+};
+const functionVariable = (arg1, arg2, ...args) => {
+    // traitement de la fonction
+    return "valeur à retourner"
+}
+
+const nullVariable = null;
+const undefinedVariable = undefined;
