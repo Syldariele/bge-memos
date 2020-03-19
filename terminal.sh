5985f998ff11572763601d840854e1ef23a0332c
Olivier Mangin <olivier@make-it.fr>
2020-03-19 09:37:39 +0100

    add terminal.sh

---
+++ terminal.sh
@@ -0,0 +1,10 @@
+# DIVERS
+cd <chemin du dossier>                                  # se déplacer
+node <nom du fichier>                                   # lancer un script node
+
+# GIT
+git init                                                # initialiser un projet git
+git add <nom du fichier / dossier>                      # ajouter les modifications au prochain commit
+git commit -m "<message à attacher au commit>"          # sauvegardé les modification précédement ajoutées
+git status                                              # affiche l'état de mon projet git à cet instant
+git log                                                 # affiche l'historique des commits
